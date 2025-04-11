e78f5386b398b65dad6776673febfb007a6d677c
Vivek Reddy <katamvivek527@gmail.com>
2025-04-11 17:08:01 +0530

    logout

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,9 +7,14 @@ const router= express.Router();
 router.post("/login",login);
 router.post('/signup',signup);
 router.get('/logout',(req,res)=>{
-    res.clearCookie('token');
-    console.log("cleared cookie");
-    res.status(200).json({msg:"Cookie delted successfully"});
+    res.clearCookie('token', {
+        httpOnly: true,
+        sameSite: "Lax", // or "None" if cross-origin
+        secure: false, // true if using HTTPS
+      });
+      console.log("cleared cookie");
+      res.status(200).json({ msg: "Cookie deleted successfully" });
+     
 })
 
 
